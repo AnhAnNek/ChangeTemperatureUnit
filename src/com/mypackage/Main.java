85ea40e61de0d645a8e4c2b789b3486093931c59
AnhAnNek <info.vananne@gmail.com>
2022-06-24 21:39:06 +0700

    Change

--- src/com/mypackage/Main.java
+++ src/com/mypackage/Main.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double degreeInFahrenheit = scanner.nextDouble();
+        double answer = convertFahrenheitToCelsius(degreeInFahrenheit);
+        System.out.println(answer);
+    }
 
+    public static double convertFahrenheitToCelsius(double degreeInFahrenheit) {
+        return (5 * (degreeInFahrenheit - 32)) / 9;
     }
 }
